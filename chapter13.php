13a2d44540d6d4e5018584591a4d6e8f02c94498
Nimit More <nimitmore27@gmail.com>
2023-01-08 00:53:18 +0530

    changes

--- chapter13.php
+++ chapter13.php
@@ -47,7 +47,6 @@ $_GET['next'] = 'chapter14.php';
     $password = "";
     $database = "winternShip";
     $con = mysqli_connect($servername,$username,$password,$database);
-    if(!$con){
     if($con){
         die("Connection Unsuccessful");
     }
